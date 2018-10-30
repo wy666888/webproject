f9ff9f68fe7c1424d6da87847d16afac5ce6d553
wy666888 <flab100@126.com>
2018-10-30 16:11:26 +0800

    字符

--- src/main/java/com/test/controller/FreemarkerController.java
+++ src/main/java/com/test/controller/FreemarkerController.java
@@ -8,9 +8,21 @@ public class FreemarkerController {
         
     @RequestMapping("/hi")    
     public String sayHello(ModelMap map){    
-        System.out.println("say hi ……");    
+        System.out.println("say hi ……");
     
         map.put("name", "kimi");    
+        map.put("cars", "<select>"+
+        		"<option value =\"volvo\">Volvo</option>"+
+        		"<option value =\"saab\" selected>Saab</option>"+
+        		"<option value=\"opel\">Opel</option>"+
+        		"<option value=\"audi\">Audi</option>"+
+        		"</select>");    
+        map.put("fruit", "<select>"+
+        		"<option value =\"volvo\">苹果</option>"+
+        		"<option value =\"saab\" selected>香蕉</option>"+
+        		"<option value=\"opel\">橘子</option>"+
+        		"<option value=\"audi\">花生</option>"+
+        		"</select>");    
     
         return "/hi.ftl";    
     }    
--- src/main/webapp/WEB-INF/ftl/hi.ftl
+++ src/main/webapp/WEB-INF/ftl/hi.ftl
@@ -1,7 +1,10 @@
 <pre name="code" class="java"><html>    
 <body>    
-    <h1>holle world ${name}</h1><br/>    
-    1 + 1 = 3? ${(1 != 1)?string("yes", "no")}    
+    <h1>holle world ${name}</h1><br/> 
+    ${cars}<br/>   
+    ${fruit}<br/>   
+    1 + 1 = 3? ${(1 != 1)?string("yes", "no")}  
+      
 </body>    
 </html>   
 </pre>  
