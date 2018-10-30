859e77fd696738e874943c4a9a9816789d9f516c
wy666888 <flab100@126.com>
2018-10-30 18:52:32 +0800

    tag

--- src/main/java/com/test/controller/FreemarkerController.java
+++ src/main/java/com/test/controller/FreemarkerController.java
@@ -1,7 +1,9 @@
 package com.test.controller;
 import org.springframework.stereotype.Controller;  
 import org.springframework.ui.ModelMap;  
-import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.test.tag.UpperDirective;  
   
 @Controller    
 public class FreemarkerController {    
@@ -25,6 +27,24 @@ public class FreemarkerController {
         		"</select>");    
     
         return "/hi.ftl";    
+    }    
+    @RequestMapping("/toUpper")    
+    public String toUpper(ModelMap map){    
+    	
+//    	map.put("name", "kimi");    
+//    	map.put("cars", "<select>"+
+//    			"<option value =\"volvo\">Volvo</option>"+
+//    			"<option value =\"saab\" selected>Saab</option>"+
+//    			"<option value=\"opel\">Opel</option>"+
+//    			"<option value=\"audi\">Audi</option>"+
+//    			"</select>");    
+//    	map.put("fruit", "<select>"+
+//    			"<option value =\"volvo\">苹果</option>"+
+//    			"<option value =\"saab\" selected>香蕉</option>"+
+//    			"<option value=\"opel\">橘子</option>"+
+//    			"<option value=\"audi\">花生</option>"+
+//    			"</select>");    
+    	return "/upper.ftl";    
     }    
     
 }    
---
+++ src/main/webapp/WEB-INF/ftl/upper.ftl
@@ -0,0 +1,11 @@
+
+foo
+<@upper>
+  bar
+  <#-- All kind of FTL is allowed here -->
+  <#list ["red", "green", "blue"] as color>
+    ${color}
+  </#list>
+  baaz
+</@upper>
+wombat
