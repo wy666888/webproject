f7c1ec51e59007e7fd5406ed08afb82e3621e54b
admin <admin@Lenovo-PC>
2020-09-12 14:30:18 +0800

    js

---
+++ src/main/webapp/index/js/042.js
@@ -0,0 +1,3 @@
+//js的执行顺序  和本外部文件被引入的位置有关的
+document.getElementById('div1').innerHTML = 'Hello';
+document.getElementById('div2').innerHTML = 'Hello';
---
+++ src/main/webapp/index/js/043.js
@@ -0,0 +1,5 @@
+//js的执行顺序  预执行
+window.onload=function(){
+	document.getElementById('div1').innerHTML = 'Hello';
+	document.getElementById('div2').innerHTML = 'Hello';
+}
